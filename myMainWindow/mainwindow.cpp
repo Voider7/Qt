28340b82607fa8dffac59f718905688ea95d7b1e
Voider7 <chenyuqitop@gmail.com>
2018-07-24 22:15:40 +0800

    class 5 from http://www.qter.org/portal.php?mod=view&aid=26

---
+++ myMainWindow/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QPushButton>
+#include <QLineEdit>
+#include <QGridLayout>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPushButton *btn = new QPushButton(this);
+    QLineEdit *le = new QLineEdit(this);
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(btn, 0, 0, 1, 1);
+    layout->addWidget(le, 0, 1, 1, 2);
+    layout->addWidget(ui->textEdit, 1, 0, 1, 3);
+    ui->centralWidget->setLayout(layout);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
